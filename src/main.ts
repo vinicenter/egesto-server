0d3f95e852cf028410cad5ffac7091654582b4e0
vinicenter <viniemidiobosi@gmail.com>
2023-07-12 21:27:33 -0300

    feat(core): implements cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
