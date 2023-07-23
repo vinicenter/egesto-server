bff5924100fc7de60a6d810db665fd5efa2848cc
vinicenter <viniemidiobosi@gmail.com>
2023-07-23 15:32:21 -0300

    feat(core): implements mongodb error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { AllExceptionsFilter } from './validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('/api/v1');
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
