69455cc52afd59ac9d623d9d39919733427af547
vinicenter <viniemidiobosi@gmail.com>
2023-07-24 00:09:19 -0300

    feat(core): temp remove exceptions filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { AllExceptionsFilter } from './validation-error.filter';
+// import { AllExceptionsFilter } from './validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api/v1');
-  app.useGlobalFilters(new AllExceptionsFilter());
+  // app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors();
 
   await app.listen(3000);
